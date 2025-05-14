c40d6fea177a0067663d513cee8478530a72d63d
boboloveai <wangbo20070412@gmail.com>
2025-05-14 22:33:25 +0800

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+from transformers import AutoTokenizer,AutoModelForSeq2SeqLM
+
+model_name = "facebook/blenderbot-400M-distill"
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+convertsation_history = []
+
+while True:
+    history_string = "\n".join(convertsation_history)
+
+    input_text = input("> ")
+
+    inputs = tokenizer.encode_plus(history_string,input_text,return_tensors="pt")
+    #print(inputs)
+
+    tokenizer.pretrained_vocab_files_map
+
+    outputs = model.generate(**inputs)
+    #print(outputs)
+
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    print(response)
+
+    convertsation_history.append(input_text)
+    convertsation_history.append(response)
+    print(convertsation_history)
+
